a16d00ed2f8cb6e253b97b0d012ba98f95f6cbf0
Akula Balaji <balajibalusbvb@gmail.com>
2026-07-30 20:08:11 +0530

    updated all files

--- hate_speech/entity/artifact_entity.py
+++ hate_speech/entity/artifact_entity.py
@@ -2,5 +2,5 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifacts:
-    imbalance_data_file_path;str
+    imbalance_data_file_path:str
     raw_data_file_path:str
--- hate_speech/logger/__init__.py
+++ hate_speech/logger/__init__.py
@@ -5,9 +5,9 @@ from from_root import from_root
 from datetime import datetime
 
 
-LOG_FILE=f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path=os.path.join(os.getcwd(),"log",LOG_FILE)
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 
 os.makedirs(logs_path,exist_ok=True)
 
